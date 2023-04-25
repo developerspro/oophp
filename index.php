bdbe57b7359424304a1d0acf7f89be5fc6fc5a65
Jeferson Ferreira <Jeferson Ferreira>
2023-04-25 18:01:39 -0300

    criação da classes iniciais

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php 
+include "Aluno.php";
+
+$em = new Aluno();
+$et = new Aluno();
+$etpi = new Aluno();
+
+$em->nome = "Huguinho";
+$em->curso = "Ensino Médio";
+$em->email="@";
+
+$et->nome = "Zezinho";
+$et->curso = "Informática para Internet";
+
+$etpi->nome = "Luizinho";
+$etpi->curso = "Técnico Integral";
+
+echo $em->curso."<br>";
+echo $em->nome."<br>";
+echo $em->email;
+
+echo "<hr>";
+
+echo $et->curso."<br>";
+echo $et->nome."<br>";
+
+echo "<hr>";
+
+echo $etpi->curso."<br>";
+echo $etpi->nome."<br>";
+
+
+$etpi->perguntar("A cor do céu?");
+$em->responder("Azul amarelado em tons de cinza");
+$em->perguntar("Que dia é hoje?");
+$et->perguntar("Teve aula hoje?");
+
+if($em->matriculado==1){
+    echo "Aluno matriculado";
+} else { 
+    echo "Aluno nao matriculado";
+    $em->fazerMatricula();
+
+}
